0dd8a11b0898845ae9a920a4f9e15714eba4ec95
irmakonder <irmakondeer@hotmail.com>
2025-04-21 21:03:19 +0300

    some functions design

--- FSM.java
+++ FSM.java
@@ -1,11 +1,11 @@
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Set;
 import java.util.Scanner;
 import java.time.LocalDate;
 public class FSM {
     static ArrayList<String> symbols;
     static ArrayList<String> states;
+    static boolean logging = false;
 
     public static void main(String[] args) {
         LocalDate currentDate = LocalDate.now();
@@ -16,21 +16,45 @@ public class FSM {
             String command = scan.nextLine();
             if (command.contains(";")) {
                 String[] inputs = command.split(" ");
+                int semicolonIndex = command.indexOf(';');
+
+                String readLine = "";
+                if (semicolonIndex != -1) {
+                    readLine = command.substring(0, semicolonIndex);
+                } else {
+                    readLine += command;
+                }
+                processCommand(readLine);
             } else {
                 System.out.println("Warning: ; is excepted.");
             }
-            int semicolonIndex = command.indexOf(';');
-
-            String readLine = "";
-            if (semicolonIndex != -1) {
-                readLine = command.substring(0, semicolonIndex);
-            } else {
-                readLine += command;
-            }
-            processCommand();
         }
     }
-    public static void processCommand() {
-
+    public static void processCommand(String prompt) {
+        String[] input = prompt.split(" ");
+        switch (input[0]) {
+            case "exit":
+                System.out.println("TERMINATED BY USER");
+                System.exit(0);
+                break;
+            case "symbols":
+                break;
+            case "initial-state":
+                break;
+            case "final-state":
+                break;
+            case "log":
+                break;
+            case "states":
+                break;
+            case "transitions":
+                break;
+            case "print":
+                break;
+            case "compile":
+                break;
+            case "load":
+                break;
+        }
     }
 }
