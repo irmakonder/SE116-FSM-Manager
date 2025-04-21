fe3322a2f22f6b09e8a41d0e4479efe73ae690bd
IlkerGedik <ilkergedik2@gmail.com>
2025-04-21 19:52:01 +0300

    main

--- FSM.java
+++ FSM.java
@@ -1,5 +1,20 @@
-public class FSM {
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Scanner;
+import java.time.LocalDate;
 
-    }
+public class FSM {
+    static Set<Character> symbols;
+    static Set<String> states;
+    static String initialStates;
+    static Set<String> finalStates;
+    static Map<String , Map<Character , String>> transition;
+
+   public static void main(String[] args) {
+       LocalDate currentDate = LocalDate.now();
+
+
+   }
 }
