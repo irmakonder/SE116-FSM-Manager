c23e61ad57ada086c76a33deeeb6006dd0f6f405
yvzmtafsr <yvzmeteafsar@hotmail.com>
2025-04-21 20:40:06 +0300

    main design

--- FSM.java
+++ FSM.java
@@ -1,20 +1,36 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.Scanner;
 import java.time.LocalDate;
-
 public class FSM {
-    static Set<Character> symbols;
-    static Set<String> states;
-    static String initialStates;
-    static Set<String> finalStates;
-    static Map<String , Map<Character , String>> transition;
+    static ArrayList<String> symbols;
+    static ArrayList<String> states;
 
-   public static void main(String[] args) {
-       LocalDate currentDate = LocalDate.now();
+    public static void main(String[] args) {
+        LocalDate currentDate = LocalDate.now();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("FSM DESIGNER<verisonNo> " + currentDate);
+        while(true) {
+            System.out.print("? ");
+            String command = scan.nextLine();
+            if (command.contains(";")) {
+                String[] inputs = command.split(" ");
+            } else {
+                System.out.println("Warning: ; is excepted.");
+            }
+            int semicolonIndex = command.indexOf(';');
 
+            String readLine = "";
+            if (semicolonIndex != -1) {
+                readLine = command.substring(0, semicolonIndex);
+            } else {
+                readLine += command;
+            }
+            processCommand();
+        }
+    }
+    public static void processCommand() {
 
-   }
+    }
 }
